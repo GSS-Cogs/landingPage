44bd111c6362adfb0b305ddc3f81c365c5719304
Alex Tucker <alex@floop.org.uk>
2020-08-19 17:34:24 +0100

    Bind to 0.0.0.0. Better errors.

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,5 @@ FROM python:3.7
 COPY Pipfile.lock Pipfile app.py /app/
 WORKDIR /app
 RUN pip install pipenv ; pipenv install --system
-CMD flask run
+EXPOSE 5000
+CMD flask run --host=0.0.0.0
--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from gssutils import Scraper
+from gssutils.scrape import MetadataError
 from markdown import markdown
 
 app = Flask(__name__)
@@ -10,8 +11,12 @@ CORS(app)
 @app.route('/')
 def index():
     landingPage = request.args.get('landingPage')
-    scraper = Scraper(landingPage)
     try:
+        scraper = Scraper(landingPage)
         return markdown(scraper._repr_markdown_())
     except Exception as err:
-        return markdown(str(err))
+        error_type = {
+            NotImplementedError: 'Not Implemented',
+            MetadataError: 'Metadata Error',
+        }.get(type(err), 'Error')
+        return markdown(f"# {error_type}\n\n{err}")
